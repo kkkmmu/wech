db9ebeb3f07c0b99af6623f3d0f3dff1d2f00d37
lamproae <lwnope@163.com>
2017-05-23 06:35:14 -0700

    Add send email

---
+++ .prepare/mail.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"net/smtp"
+	"strings"
+)
+
+const (
+	HOST        = "smtp.163.com"
+	SERVER_ADDR = "smtp.163.com:25"
+	USER        = "lwnope@163.com" //发送邮件的邮箱
+	PASSWORD    = "password"       //发送邮件邮箱的密码
+)
+
+type Email struct {
+	to      string
+	subject string
+	msg     string
+}
+
+func NewEmail(to, subject, msg string) *Email {
+	return &Email{to: to, subject: subject, msg: msg}
+}
+
+func SendEmail(email *Email) error {
+	auth := smtp.PlainAuth("", USER, PASSWORD, HOST)
+	sendTo := strings.Split(email.to, ";")
+	done := make(chan error, 1024)
+
+	go func() {
+		defer close(done)
+		for _, v := range sendTo {
+
+			str := strings.Replace("From: "+USER+"~To: "+v+"~Subject: "+email.subject+"~~", "~", "\r\n", -1) + email.msg
+
+			err := smtp.SendMail(
+				SERVER_ADDR,
+				auth,
+				USER,
+				[]string{v},
+				[]byte(str),
+			)
+			done <- err
+		}
+	}()
+
+	for i := 0; i < len(sendTo); i++ {
+		<-done
+	}
+
+	return nil
+}
+
+func main() {
+	email := NewEmail("kkkmmu@163.com", "IBM峰会资料下载地址", "IBM峰会资料下载地址IBM峰会资料下载地址IBM峰会资料下载地址")
+
+	err := SendEmail(email)
+
+	fmt.Println(err)
+
+}
